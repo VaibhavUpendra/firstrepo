938ae04d39a906b55642a4e56b5e2092fe3b8419
VaibhavUpendra <nuclearweapon211@gmail.com>
2024-09-23 13:52:47 +0530

    first commit

---
+++ mathmodule.py
@@ -0,0 +1,23 @@
+import math
+ex_var = math.sqrt(49)
+pow_var = math.pow(3,2)
+ceil_var = math.ceil(6.6)
+flr_var = math.floor(5.7)
+abs_var = math.fabs(-20)
+fct_var = math.factorial(5)
+sin_var = math.sin(math.pi/2)
+cos_var =  math.cos(math.pi/3)
+tan_var = math.tan(math.pi/2)
+lg2_var = math.log2(5)
+lg10_var = math.log10(23)
+deg_var = math.degrees(math.pi/2)
+rad_var = math.radians(45)
+eul_var = math.e
+inf_var = math.inf
+nan_var = math.nan
+gcd_var = math.gcd(5,6,7)
+lcm_var = math.lcm(5,10,20)
+
+
+
+print(lcm_var)
